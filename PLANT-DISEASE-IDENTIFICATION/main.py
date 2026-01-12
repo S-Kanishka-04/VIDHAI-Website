3d35e734829a262e2e77f8c138e11309754388aa
Kanishka S <kanishkashankar71@gmail.com>
2026-01-12 18:03:20 +0530

    Refactor model prediction and improve image handling

--- PLANT-DISEASE-IDENTIFICATION/main.py
+++ PLANT-DISEASE-IDENTIFICATION/main.py
@@ -1,56 +1,69 @@
 import streamlit as st
 import tensorflow as tf
 import numpy as np
-def model_prediction(test_image):
-    model = tf.keras.models.load_model("trained_plant_disease_model.keras")
-    image = tf.keras.preprocessing.image.load_img(test_image,target_size=(128,128))
-    input_arr = tf.keras.preprocessing.image.img_to_array(image)
-    input_arr = np.array([input_arr]) #convert single image to batch
-    predictions = model.predict(input_arr)
-    return np.argmax(predictions) #return index of max element
-
-#Sidebar
-st.sidebar.title("VIDHAI")
-app_mode = st.sidebar.selectbox("Select Page",["HOME","DISEASE RECOGNITION"])
-#app_mode = st.sidebar.selectbox("Select Page",["Home","About","Disease Recognition"])
-
-# import Image from pillow to open images
 from PIL import Image
-img = Image.open("Diseases.png")
+import os
 
-# display image using streamlit
-# width is used to set the width of an image
-st.image(img)
+# ---------- Paths (VERY IMPORTANT) ----------
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+MODEL_PATH = os.path.join(BASE_DIR, "trained_plant_disease_model.keras")
+IMAGE_PATH = os.path.join(BASE_DIR, "Diseases.png")
 
-#Main Page
-if(app_mode=="HOME"):
-    st.markdown("<h1 style='text-align: center;'>SMART DISEASE DETECTION", unsafe_allow_html=True)
-    
-#Prediction Page
-elif(app_mode=="DISEASE RECOGNITION"):
+# ---------- Model Prediction Function ----------
+def model_prediction(uploaded_file):
+    model = tf.keras.models.load_model(MODEL_PATH)
+
+    image = Image.open(uploaded_file).resize((128, 128))
+    image = np.array(image)
+    image = image / 255.0
+    image = np.expand_dims(image, axis=0)
+
+    predictions = model.predict(image)
+    return np.argmax(predictions)
+
+# ---------- Sidebar ----------
+st.sidebar.title("VIDHAI")
+app_mode = st.sidebar.selectbox("Select Page", ["HOME", "DISEASE RECOGNITION"])
+
+# ---------- Show Banner Image ----------
+img = Image.open(IMAGE_PATH)
+st.image(img, width=400)
+
+# ---------- HOME ----------
+if app_mode == "HOME":
+    st.markdown("<h1 style='text-align: center;'>SMART DISEASE DETECTION</h1>", unsafe_allow_html=True)
+
+# ---------- DISEASE RECOGNITION ----------
+elif app_mode == "DISEASE RECOGNITION":
     st.header("DISEASE RECOGNITION")
-    test_image = st.file_uploader("Choose an Image:")
-    if(st.button("Show Image")):
-        st.image(test_image,width=4,use_column_width=True)
-    #Predict button
-    if(st.button("Predict")):
-        st.snow()
-        st.write("Our Prediction")
-        result_index = model_prediction(test_image)
-        #Reading Labels
-        class_name = ['Apple___Apple_scab', 'Apple___Black_rot', 'Apple___Cedar_apple_rust', 'Apple___healthy',
-                    'Blueberry___healthy', 'Cherry_(including_sour)___Powdery_mildew', 
-                    'Cherry_(including_sour)___healthy', 'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot', 
-                    'Corn_(maize)___Common_rust_', 'Corn_(maize)___Northern_Leaf_Blight', 'Corn_(maize)___healthy', 
-                    'Grape___Black_rot', 'Grape___Esca_(Black_Measles)', 'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)', 
-                    'Grape___healthy', 'Orange___Haunglongbing_(Citrus_greening)', 'Peach___Bacterial_spot',
-                    'Peach___healthy', 'Pepper,_bell___Bacterial_spot', 'Pepper,_bell___healthy', 
-                    'Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy', 
-                    'Raspberry___healthy', 'Soybean___healthy', 'Squash___Powdery_mildew', 
-                    'Strawberry___Leaf_scorch', 'Strawberry___healthy', 'Tomato___Bacterial_spot', 
-                    'Tomato___Early_blight', 'Tomato___Late_blight', 'Tomato___Leaf_Mold', 
-                    'Tomato___Septoria_leaf_spot', 'Tomato___Spider_mites Two-spotted_spider_mite', 
-                    'Tomato___Target_Spot', 'Tomato___Tomato_Yellow_Leaf_Curl_Virus', 'Tomato___Tomato_mosaic_virus',
-                      'Tomato___healthy']
 
-        st.success("Model is Predicting it's a {}".format(class_name[result_index]))
+    test_image = st.file_uploader("Choose an Image", type=["jpg", "png", "jpeg"])
+
+    if test_image is not None:
+        st.image(test_image, use_column_width=True)
+
+        if st.button("Predict"):
+            st.snow()
+            st.write("Our Prediction")
+
+            result_index = model_prediction(test_image)
+
+            class_name = [
+                'Apple___Apple_scab', 'Apple___Black_rot', 'Apple___Cedar_apple_rust', 'Apple___healthy',
+                'Blueberry___healthy', 'Cherry_(including_sour)___Powdery_mildew',
+                'Cherry_(including_sour)___healthy', 'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot',
+                'Corn_(maize)___Common_rust_', 'Corn_(maize)___Northern_Leaf_Blight',
+                'Corn_(maize)___healthy', 'Grape___Black_rot', 'Grape___Esca_(Black_Measles)',
+                'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)', 'Grape___healthy',
+                'Orange___Haunglongbing_(Citrus_greening)', 'Peach___Bacterial_spot',
+                'Peach___healthy', 'Pepper,_bell___Bacterial_spot', 'Pepper,_bell___healthy',
+                'Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy',
+                'Raspberry___healthy', 'Soybean___healthy', 'Squash___Powdery_mildew',
+                'Strawberry___Leaf_scorch', 'Strawberry___healthy', 'Tomato___Bacterial_spot',
+                'Tomato___Early_blight', 'Tomato___Late_blight', 'Tomato___Leaf_Mold',
+                'Tomato___Septoria_leaf_spot', 'Tomato___Spider_mites Two-spotted_spider_mite',
+                'Tomato___Target_Spot', 'Tomato___Tomato_Yellow_Leaf_Curl_Virus',
+                'Tomato___Tomato_mosaic_virus', 'Tomato___healthy'
+            ]
+
+            st.success(f"Model predicts: **{class_name[result_index]}**")
